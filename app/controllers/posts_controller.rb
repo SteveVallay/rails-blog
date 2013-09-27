e38ce651e79ceb523b47e06377240e879166f96d
zhibinw <zhibinwang.q@gmail.com>
2013-09-27 17:01:45 +0800

    add links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,11 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params[:post].permit(:title, :text))
 
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
   def show
     @post = Post.find(params[:id])
