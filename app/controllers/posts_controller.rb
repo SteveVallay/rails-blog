a91470197b70891e38efdd2d791b73cf6d0240e9
zhibinw <zhibinwang.q@gmail.com>
2013-09-27 18:01:42 +0800

    add delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,12 @@ class PostsController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @post =Post.find_by_id(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
   #private
   #  def post_params
   #    params.require(:post).permit(:title,:text)
